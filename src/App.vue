031b0261bc4937c27d462bc209ca92fb5501b4b1
Dmitriy Burlak <dmburlak@MacBook-Air-Dmitriy.local>
2022-08-04 22:15:41 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="title">{{title}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "start"
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    .title {
+        color: red;
+    }
+</style>
